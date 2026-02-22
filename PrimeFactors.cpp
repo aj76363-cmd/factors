eb0feebe2e249fadd49184b3eb675de5b3597846
aj76363-cmd <aj76363@mail.harpercollege.edu>
2026-02-22 08:54:40 -0600

    with recursion

--- PrimeFactors.cpp
+++ PrimeFactors.cpp
@@ -5,83 +5,86 @@
 using namespace std;
 
 class factz {
+    void facth(int n, int div, vector<int>& fact) {
+
+        if (n == 1) {
+
+            return;
+        }
+
+        if (n % div == 0) {  
+            fact.push_back(div);
+
+            facth(n / div, div, fact);
+        }
+        else {
+            facth(n, div + 1, fact);
+
+        }
+    }
 public:
-    // Compute prime fact
+
 
     vector<int> primef(int n) {
 
         vector<int> fact;
 
         if (n <= 1) {
+
             return fact;
         }
 
-        int div = 2;
-
-        while (n > 1) {
-            if (n % div == 0) {  //if no remainder we pass this else w incriment the divisor
-                fact.push_back(div);
-
-                n = n / div;
-            }
-            else {
-                div++;
-            }
-        }
+        facth(n, 2, fact);
 
         return fact;
     }
+
     void printf(const vector<int>& fact) {
 
-        cout << "\n[ ";//prints a vector of fact
-        for (int x : fact) {
 
-            cout << x << ","; //seperates numbers
+        cout << "\n[ ";
+        for (int x : fact) {
+            cout << x << ",";
         }
         cout << "]\n";
     }
+
+
 };
 
 int main() {
     factz f;
+
     while (true) {
         int input;
         cout << "\n Would you like to factor a number? (0) no\n";
         cin >> input;
-        if (input == 0)break;
-       
-        int number;
-        cout << "\n Enter a number: ";
+        if (input == 0) break;
 
+        int number;
+        cout << "\n number: ";
         cin >> number;
 
         vector<int> fact = f.primef(number);
 
-        cout << "\n fact of" << number << " = ";
+        cout << "\n fact of " << number << " = ";
         f.printf(fact);
-
-        cout << "\n";
-
     }
 
     vector<int> result = f.primef(0);
     cout << "\nshould=[]\t Got: ";
     f.printf(result);
-    cout << "\n";
 
     result = f.primef(1);
     cout << "\nshould=[]\t Got: ";
     f.printf(result);
-    cout << "\n";
 
     result = f.primef(9);
-    cout << "\nshould=[3, 3,]\t Got: ";
+    cout << "\nshould=[3,3,]\t Got: ";
     f.printf(result);
-    cout << "\n";
 
     return 0;
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
